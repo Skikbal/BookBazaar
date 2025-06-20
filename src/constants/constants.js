1b76de0ceff812d40b2797d25607ef312b29f770
Skikbal <skikbalali02@gmail.com>
2025-06-20 21:37:35 +0530

    feat:completed all the models

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,3 +5,23 @@ export const userRoleEnum = {
 };
 
 export const availableRoles = Object.keys(userRoleEnum);
+
+export const ratingEnum = {
+  ONE: 1,
+  TWO: 2,
+  THREE: 3,
+  FOUR: 4,
+  FIVE: 5,
+};
+
+export const availableRatings = Object.keys(ratingEnum);
+
+export const orderStatusEnum = {
+  "PENDING": "pending",
+  "DELIVERED": "delivered",
+  "CANCELLED": "cancelled",
+  "SHIPPED": "shipped",
+  "OUT FOR DELIVERY": "out for delivery",
+};
+
+export const availbaleOrderStatus = Object.keys(orderStatusEnum);
---
+++ src/models/order.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import { orderStatusEnum, availbaleOrderStatus } from "../constants/constants.js";
+const orderSchema = new Schema({
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Books",
+      required: true,
+    },
+  ],
+  orderedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  totalPrice: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: orderStatusEnum,
+    default: availbaleOrderStatus.PENDING,
+  },
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+
+}, { timestamps: true });
+export const Order = mongoose.model("Orders", orderSchema);
