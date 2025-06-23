a00ef605fede97a59dd47ef1a17ec3c19c9da0c2
Skikbal <skikbalali02@gmail.com>
2025-06-23 23:04:30 +0530

    feat: Implemneted user login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,4 +32,36 @@ const registrationHandler = AsyncHandler(async (req, res) => {
 
   return res.status(201).json(new ApiResponse(201, "User created successfully", user));
 });
-export { registrationHandler };
+
+// login handler
+const loginHandler = AsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  if (!user.isVerified) {
+    throw new ApiError(400, "User is not verified");
+  }
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid username or password");
+  }
+  const { accessToken, refreshToken } = await user.generateToken();
+  user.refreshToken = refreshToken;
+  await user.save();
+  const userData = await User.findById(user._id).select({ password: 0, refreshToken: 0, verificationToken: 0, verificationTokenExpiry: 0 });
+  const cookiesOptions = {
+    httpOnly: true,
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookiesOptions)
+    .cookie("refreshToken", refreshToken, cookiesOptions)
+    .json(new ApiResponse(200, "Login successful", userData));
+});
+
+export { registrationHandler, loginHandler };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registrationHandler } from "../controllers/auth.controller.js";
+import { loginHandler, registrationHandler } from "../controllers/auth.controller.js";
 import validate from "../middleware/validation.middleware.js";
 import { registerSchema } from "../validators/auth.validator.js";
 
 const authRouter = Router();
 
 authRouter.route("/signup").post(validate(registerSchema), registrationHandler);
+authRouter.route("/login").post(loginHandler);
 
 export default authRouter;
